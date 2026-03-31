87348437dd6d7701fdb48466a0900c06b6632d16
Dor Hakim <dorhakim100@gmail.com>
2026-03-31 19:04:29 +0300

    Move vouchers array into it's own file

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
 
 
-const vouchers = [
-  { serialNumber: "24062574", fullName: "אורי קסטיאל" },
-  { serialNumber: "51822977", fullName: "מירב כץ" },
-  { serialNumber: "55892925", fullName: "גאורגי מקוקישווילי" },
-  { serialNumber: "36262045", fullName: "הילה דפני" },
-  { serialNumber: "91267367", fullName: "אלי הרוש" },
-  { serialNumber: "85424300", fullName: "כרמל פוליאק" },
-  { serialNumber: "83991842", fullName: "לולי אמירי" },
-  { serialNumber: "50906442", fullName: "אדווה ברעם" },
-  { serialNumber: "71023356", fullName: "אלה-יה איסמלון" },
-  { serialNumber: "11174006", fullName: "גליה אור ליאוז" },
-  { serialNumber: "51744146", fullName: "מירב דפני" },
-  { serialNumber: "34994370", fullName: "משה זיסקינד" },
-  { serialNumber: "94574914", fullName: "נעמי רמון" },
-  { serialNumber: "74276854", fullName: "סמדר דינור" },
-  { serialNumber: "87921190", fullName: "תמי אריאלי" }
-];
+import { vouchers } from './vouchers.js';
 
 
 function onInit(){
--- js/server.js
+++ js/server.js
@@ -3,6 +3,8 @@ import { chromium } from 'playwright';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
+import { vouchers } from './vouchers.js';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,23 +12,6 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
 
-const vouchers = [
-  { serialNumber: "24062574", fullName: "אורי קסטיאל" },
-  { serialNumber: "51822977", fullName: "מירב כץ" },
-  { serialNumber: "55892925", fullName: "גאורגי מקוקישווילי" },
-  { serialNumber: "36262045", fullName: "הילה דפני" },
-  { serialNumber: "91267367", fullName: "אלי הרוש" },
-  { serialNumber: "85424300", fullName: "כרמל פוליאק" },
-  { serialNumber: "83991842", fullName: "לולי אמירי" },
-  { serialNumber: "50906442", fullName: "אדווה ברעם" },
-  { serialNumber: "71023356", fullName: "אלה-יה איסמלון" },
-  { serialNumber: "11174006", fullName: "גליה אור ליאוז" },
-  { serialNumber: "51744146", fullName: "מירב דפני" },
-  { serialNumber: "34994370", fullName: "משה זיסקינד" },
-  { serialNumber: "94574914", fullName: "נעמי רמון" },
-  { serialNumber: "74276854", fullName: "סמדר דינור" },
-  { serialNumber: "87921190", fullName: "תמי אריאלי" }
-];
 
 app.use(express.static(join(__dirname, '..')));
 
